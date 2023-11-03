64c1e83a39c673f94ec17385e2fc839390c35777
Lebedeva Svetlana <svetlana-swanson@yandex.ru>
2023-11-03 12:55:14 +0300

    Добавьте файлы проекта.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EverydayIsArt.Controllers
+{
+    public class HomeController : Controller
+    {
+        public HomeController()
+        {
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
