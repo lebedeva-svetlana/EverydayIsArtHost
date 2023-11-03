15eea916a34cd934d9ff89ade36d0e8ffeed8ead
Lebedeva Svetlana <svetlana-swanson@yandex.ru>
2023-11-03 16:16:39 +0300

    Add PostViewModel.

---
+++ ViewModels/PostViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace EverydayIsArt.ViewModels
+{
+    public class PostViewModel
+    {
+        public string Title { get; set; }
+
+        public string? Text { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string SourceUrl { get; set; }
+    }
+}
