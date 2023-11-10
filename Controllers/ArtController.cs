5764507c8a9b6db306e17267d51441429c697074
Lebedeva Svetlana <svetlana-swanson@yandex.ru>
2023-11-10 16:51:37 +0300

    Rename controllers and views.

--- Controllers/ArtController.cs
+++ Controllers/ArtController.cs
@@ -4,16 +4,21 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace EverydayIsArt.Controllers
 {
-    public class HomeController : Controller
+    public class ArtController : Controller
     {
         private IArtService _artService;
 
-        public HomeController(IArtService artService)
+        public ArtController(IArtService artService)
         {
             _artService = artService;
         }
 
-        public async Task<IActionResult> Index()
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Random(string source)
         {
             Art art = await _artService.GetArt();
             return View(art);
--- Program.cs
+++ Program.cs
@@ -28,6 +28,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Art}/{action=Index}/{source?}");
 
 app.Run();
