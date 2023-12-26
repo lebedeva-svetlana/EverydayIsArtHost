a198002196dd942375073592f5ec7600a5bb06e6
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2023-12-26 12:38:46 +0300

    Change sass variables names.

---
+++ src/components/ShareButton.jsx
@@ -0,0 +1,21 @@
+import { useMediaQuery } from "react-responsive";
+import { useState, useEffect } from "react";
+
+/*import Icon from '../assets/images/colorScheme.svg';*/
+
+import '../styles/ShareButton.scss';
+
+//function ShareButton() {
+//    const [isDark, setIsDark] = useState(false);
+//    function handleClick() {
+//        setIsDark(!isDark);
+//    }
+
+//    return (
+//        <button onClick={handleClick} className="change-color-scheme">
+//            <img src={Icon} className="color-scheme-icon"></img>
+//        </button >
+//    );
+//}
+
+//export default ColorSchemeButton;
