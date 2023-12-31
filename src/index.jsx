a6670330d892fc2054eedc472b68a0739bc400bb
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2023-12-31 19:21:21 +0300

    Change default route  path.

--- src/index.jsx
+++ src/index.jsx
@@ -21,7 +21,7 @@ const router = createBrowserRouter([
         ),
         children: [
             {
-                path: "/",
+                path: "",
                 element: (
                     <About></About>
                 )
