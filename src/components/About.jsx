67e3beff24fba024be15705268cf97906098726f
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-01-01 17:46:42 +0300

    Remove br from about.

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -8,7 +8,7 @@ function About() {
         <div className="about">
             <img src={Logo} alt="Логотип EverydayIsArt" className="about-logo"></img>
             <div className="about-text">
-                <h2 className="about-title">Открыйвайте для себя <br></br>новое c <span className="about-project-name-green">Everyday</span>Is<span className="about-project-name-green">Art</span></h2>
+                <h2 className="about-title">Открыйвайте для себя новое c <span className="about-project-name-green">Everyday</span>Is<span className="about-project-name-green">Art</span></h2>
                 <p className="about-info">Выберите источник и получите случайную работу из его коллекции.</p>
             </div>
         </div>
