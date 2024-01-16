6291de8d96fd18b559c05126256de144452d3ab4
lebedeva-svetlana <svetlana-swanson@yandex.ru>
2024-01-16 09:12:44 +0300

    Add login link to main component.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,6 +1,7 @@
 ﻿import { useMediaQuery } from 'react-responsive';
 
 import ColorSchemeButton from './ColorSchemeButton.jsx';
+import LoginLink from './LoginLink.jsx';
 
 import '../styles/Main.scss';
 
@@ -11,7 +12,10 @@ function Main({ children }) {
 
     return (
         <>
-            {isDesktop && <ColorSchemeButton></ColorSchemeButton>}
+            {isDesktop && <div className="main-bar">
+                <ColorSchemeButton></ColorSchemeButton>
+                <LoginLink></LoginLink>
+            </div>}
             <main className="main">
                 {children}
             </main>
